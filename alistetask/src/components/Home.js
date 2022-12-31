48c56967f330856872f9d1d732aba35d5d01ce93
Digvijay555 <digvijayrana123456@gmail.com>
2022-12-31 23:26:20 +0530

    commit 3

--- alistetask/src/components/Home.js
+++ alistetask/src/components/Home.js
@@ -33,7 +33,7 @@ const Home = () => {
     }
 
     const getRecord = async (id) => {
-        const data =[];
+        const data = [];
         const response = await fetch(`http://localhost:5000/api/user/getuser/${id}`, {
             method: 'GET',
             headers: {
@@ -42,11 +42,24 @@ const Home = () => {
 
         })
         const records = await response.json(); // parses JSON response into native JavaScript objects
-      
+
         data.push(records);
         setRecord(data);
     }
 
+    const deleteUsers = async (homeId, id) => {
+
+        //API
+
+        await fetch(`http://localhost:5000/api/user/deleteuser/${homeId}/${id}`, {
+            method: "Delete",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        getData();
+    }
 
     const saveData = () => {
         console.log(data);
@@ -62,53 +75,60 @@ const Home = () => {
         // console.log(id);
         getRecord(id.homeId);
     }
+
+    const deleteUser = (homeId, id) => {
+        deleteUsers(homeId, id);
+    }
     return (
         <>
             <div ><h1 className="header">Aliste Home Page</h1></div>
 
             <div className='container'>
                 <div className="card">
-                    <h4>HomeId</h4>
+                    {/* <h4>HomeId</h4> */}
                     <TextField id="standard-basic" label="HomeId" variant="standard" name="homeId" onChange={onChange} />
 
-                    <h4>Name</h4>
+                    {/* <h4>Name</h4> */}
                     <TextField id="standard-basic" label="UserName" variant="standard" name="name" onChange={onChange} />
-                    <h4>UserId</h4>
+                    {/* <h4>UserId</h4> */}
                     <TextField id="standard-basic" label="UserId" variant="standard" name="userId" onChange={onChange} />
                     <br />
                     <br />
                     <Button variant="contained" onClick={saveData}>SaveData</Button>
                 </div>
                 <div className='searchContainer'>
-                    <h3 className='header'>Get User Details</h3>
+                    <h3 className='Subheader'>Get User Details</h3>
+                    <br/>
                     <div className='searchdiv'>
                         <TextField id="standard-basic" label="HomeId" variant="standard" name="homeId" onChange={onChangee} />
                         <Button variant="contained" onClick={getData}>Get User Details</Button>
                     </div>
-                    <div>
-                        <table>
+                    <div className='tablediv'>
+                        <table class="table">
                             <thead>
                                 <tr>
-                                    <th>HomeId</th>
-                                    <th>User_Name</th>
-                                    <th>User_Id</th>
+                                    <th >HomeId</th>
+                                    <th >User_Name</th>
+                                    <th >User_Id</th>
+                                    <th >Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                            
-                                
-                               {
-                                   record.length >0? 
-                                   record[0].user.map((ele) => {
-                                        
-                                        return(<tr>
-                                            <td>{record[0].homeId}</td>
-                                            <td>{ele.username}</td>
-                                            <td>{ele.id}</td>
-                                        </tr>)
-                                    })
-                                    :
-                                    null
+
+
+                                {
+                                    record.length > 0 ?
+                                        record[0].user.map((ele) => {
+
+                                            return (<tr>
+                                                <td>{record[0].homeId}</td>
+                                                <td>{ele.username}</td>
+                                                <td>{ele.id}</td>
+                                                <td><Button variant="contained" onClick={() => { deleteUser(record[0].homeId, ele.id) }}>DeleteUser</Button></td>
+                                            </tr>)
+                                        })
+                                        :
+                                        null
                                 }
 
                             </tbody>
